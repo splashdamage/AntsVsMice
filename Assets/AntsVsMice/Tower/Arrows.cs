5530a8cd22b4f5aea52b4028b67bd27a14804dca
Elisabeth Carpenter <EBC@Elisabeths-MacBook-Pro.local>
2012-08-02 11:29:05 -0400

    summary

--- Assets/AntsVsMice/Tower/Arrows.cs
+++ Assets/AntsVsMice/Tower/Arrows.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class Arrows : BulletShooter {
-
+	public bool normalBullet;
 	// Use this for initialization
 	void Start () {
 	
--- Assets/AntsVsMice/Tower/Cannon.cs
+++ Assets/AntsVsMice/Tower/Cannon.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class Cannon : BulletShooter {
-
+	public bool explosiveBullet;
 	// Use this for initialization
 	void Start () {
 	
