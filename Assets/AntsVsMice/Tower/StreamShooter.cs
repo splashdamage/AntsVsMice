b69b180217c0d66977184e7ea61471b47d7fd99d
Daniel Bleck <dan4795@gmail.com>
2012-08-02 11:03:47 -0400

    Added tower types

---
+++ Assets/AntsVsMice/Tower/StreamShooter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class StreamShooter : Shooter {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
