5b6f0798664fdd5eef181bbd96c88475de7f7182
Elisabeth Carpenter <EBC@Elisabeths-MacBook-Pro.local>
2012-08-03 11:07:10 -0400

    enemy damage

---
+++ Assets/AntsVsMice/Damage.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Damage {
+	public enum Type {
+		none=0,
+		direct=1,
+		explosive=2,
+		acid=3,
+		slow=4
+	}
+	public Type type;
+	
+	public float amount;
+}
---
+++ Assets/AntsVsMice/Resistance.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public class Resistance {
+	public Damage.Type type;
+	public float percent = 1;
+	public bool unTargetable = false;
+}
